339a989049411fe0cb7776ffeb6886a2180401b4
rydb <ryan.dobe@gmail.com>
2022-10-28 11:13:10 -0500

    added change that I forgot to push

--- src/model_pkg/model_pkg/model.py
+++ src/model_pkg/model_pkg/model.py
@@ -1,6 +1,52 @@
-from inspect import getsourcefile
-from os.path import abspath
-import glob
+#For importing time
+import rclpy
+import tf2_msgs
+#tf2 stuff,
+import tf2_ros
+#Need to get /Twist messages from the /cmd_vel topic from encoderless_teleop keyboard
+from geometry_msgs.msg import Quaternion, Twist
+from nav_msgs.msg import Odometry
+from rclpy.clock import ROSClock
+from rclpy.node import Node
+from scipy.spatial.transform import Rotation
+from std_msgs.msg import Float32, Int32MultiArray, String
+
+class Model():
+    """
+    A model consists of a base(non moving parts) and wheels(the moving parts)
+
+    these are split up into /base_link and /wheel_link 1 and 2 respectively. 
+    """
+    def __init__(self, stl_base, stl_wheels):
+        
+        #GEOMETRY
+        self.base = stl_base
+        self.center_of_mass = None
+
+        self.wheels = stl_wheels
+    
+    def create_base_link_from_stl(self, stl_b):
+        """
+        create the /base_link to publish from the stl_base
+        
+        a base link has:
+            /
+        """
+        
+
+    def create_wheel_links_from_stl():
+        """
+        create /wheel_link(s) to publish from the stl_wheels
+        """
+
+
+def publish_model():
+    """
+    collects all of the stls that the model uses, creates a topic, populates it with relevant information, then publishes it. to ros2.
+    """
+    
+
+    pass
 
 def main():
     print('Hi from model_pkg.')
